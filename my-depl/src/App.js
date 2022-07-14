5be6f5fbd8c8effd83d1da73f363be60dd489d1d
qqqwwwddd <qqqwwwddd1@gmail.com>
2022-07-14 17:08:30 +0900

    update test

---
+++ my-depl/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    const [msg, setMsg] = useState("");
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/deploy")
+            .then((response) => response.text())
+            .then((msg) => {
+                setMsg(msg);
+            });
+    });
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    {/* Edit <code>src/App.js</code> and save to reload. */}
+                    {msg}
+                </p>
+
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
